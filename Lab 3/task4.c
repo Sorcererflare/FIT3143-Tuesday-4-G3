0cf72be00097b43f7189ca2e7915ed07bcafe127
pcoo0009 <144221217+pcoo0009@users.noreply.github.com>
2025-09-02 17:13:08 +1000

    Update task4.c
    IDs

--- Lab 3/task4.c
+++ Lab 3/task4.c
@@ -1,4 +1,7 @@
 // task4.c
+// By: Abdullah (33861641) (16.7%), Hayden (33861889) (16.7%), Samuel (33114110) (16.7%), 
+// Hesamreza (33861544) (16.7%), Peter (33143722) (16.7%), Khang (33048258) (16.7%)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -62,3 +65,4 @@ int main(){
     
     return 0;
 }
+
