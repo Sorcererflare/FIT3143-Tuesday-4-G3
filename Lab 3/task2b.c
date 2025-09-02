284e05bb44ee45df5ae3558fe3ed2ee15875aca9
pcoo0009 <144221217+pcoo0009@users.noreply.github.com>
2025-09-02 17:12:05 +1000

    Update task2b.c
    IDs

--- Lab 3/task2b.c
+++ Lab 3/task2b.c
@@ -1,6 +1,9 @@
 // BCast is a lot simpler than Send, can send/receive through BCast and can 
 // send to all processes in a single statement
 
+// By: Abdullah (33861641) (16.7%), Hayden (33861889) (16.7%), Samuel (33114110) (16.7%), 
+// Hesamreza (33861544) (16.7%), Peter (33143722) (16.7%), Khang (33048258) (16.7%)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
